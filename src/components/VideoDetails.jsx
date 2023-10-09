c55982e2a7ea85c82622b216ea9b450da7e5fe4b
Aman Rawat <imdemario@gmail.com>
2023-10-09 21:47:02 +0530

    new

--- src/components/VideoDetails.jsx
+++ src/components/VideoDetails.jsx
@@ -66,12 +66,19 @@ const VideoDetails = () => {
                 <div className="flex flex-col lg:w-[calc(100%-350px)] xl:w-[calc(100%-400px)] px-4 py-3 lg:py-6 overflow-y-auto">
                     <div className="h-[200px] md:h-[400px] lg:h-[400px] xl:h-[550px] ml-[-16px] lg:ml-0 mr-[-16px] lg:mr-0">
                         <ReactPlayer
-                            url="https://www.youtube.com/watch?v=l6U1JB7z-I8"
+                            url={video?.video_url}
                             controls
                             width="100%"
                             height="100%"
                             style={{ backgroundColor: "#000000" }}
                             playing={true}
+                            config={{
+                                file: {
+                                    headers: {
+                                        "projectId": "f104bi07c490"
+                                    }
+                                }
+                            }}
                         />
 
                     </div>
